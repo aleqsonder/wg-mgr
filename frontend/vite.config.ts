bcba7089a844841259a5f7c604402eb21eff5648
denis <mixexployer@gmail.com>
2026-02-19 16:16:21 +0300

    config for .env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:80"
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: `http://${env.VITE_API_HOST}:${env.VITE_API_PORT}`,
+          changeOrigin: true
+        }
+      }
     }
   }
 })
