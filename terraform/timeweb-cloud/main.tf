72c2075b2c28aae5afb82b18b213f355b5efb8a6
alexsm <alexsm.w@yandex.ru>
2026-03-06 21:08:29 +0000

    cd(tf): Set up Ubuntu with Docker on timeweb.cloud

--- terraform/timeweb-cloud/main.tf
+++ terraform/timeweb-cloud/main.tf
@@ -1,30 +1,37 @@
 terraform {
   required_providers {
-	twc = {
-		source = "tf.timeweb.cloud/timeweb-cloud/timeweb-cloud"
-	}
+    twc = {
+      source = "tf.timeweb.cloud/timeweb-cloud/timeweb-cloud"
+    }
   }
   required_version = ">= 0.13"
 }
 
-data "twc_configurator" "configurator" {
-	location = "ru-1"
-	disk_type = "nvme"
+data "twc_presets" "preset" {
+  location    = "ru-1"
+  preset_type = "premium"
+  cpu         = 1
+  ram         = 1024
+
+  price_filter {
+    from = 300
+    to   = 400
+  }
 }
 
-data "twc_os" "os" {
-	name = "ubuntu"
-	version = "22.04"
+data "twc_software" "docker" {
+  name = "Docker"
+
+  os {
+    name    = "ubuntu"
+    version = "22.04"
+  }
 }
 
-resource "twc_server" "example-server" {
-	name = "Example server"
-	os_id = data.twc_os.os.id
+resource "twc_server" "wg-mgr" {
+  name        = "WireGuard Manager"
+  os_id       = data.twc_software.docker.os[0].id
+  software_id = data.twc_software.docker.id
 
-	configuration {
-		configurator_id = data.twc_configurator.configurator.id
-		disk = 1024 * 15
-		cpu = 1
-		ram = 1024
-	}
+  preset_id = data.twc_presets.preset.id
 }
