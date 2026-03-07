f5c7e1c174c43b6a970342546c37b5774f812bb5
Alexcoratt <alexsm.w@yandex.ru>
2026-03-07 05:43:48 +0000

    feat(tf): Upgrade the infrastructure
    - Add the 'WireGuard Manager' project
    - Set up floating IP

--- terraform/timeweb-cloud/main.tf
+++ terraform/timeweb-cloud/main.tf
@@ -28,10 +28,26 @@ data "twc_software" "docker" {
   }
 }
 
-resource "twc_server" "wg-mgr" {
-  name        = "WireGuard Manager"
-  os_id       = data.twc_software.docker.os[0].id
-  software_id = data.twc_software.docker.id
+resource "twc_project" "wg-mgr-project" {
+  name = "WireGuard Manager"
+}
+
+resource "twc_server" "main" {
+  name              = "Main Server"
+  os_id             = data.twc_software.docker.os[0].id
+  software_id       = data.twc_software.docker.id
+  availability_zone = "spb-3"
 
   preset_id = data.twc_presets.preset.id
+
+  project_id = resource.twc_project.wg-mgr-project.id
+}
+
+resource "twc_floating_ip" "floating-ip" {
+  availability_zone = twc_server.main.availability_zone
+
+  resource {
+    type = "server"
+    id   = twc_server.main.id
+  }
 }
