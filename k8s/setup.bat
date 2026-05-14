0effed2ec8e10ec8ec9cd21053816d4e0c01dc6c
alexsm <alexsm.w@yandex.ru>
2026-05-14 20:09:18 +0300

    fix(monitoring): Correct metrics collection & visualization
    - Rename grafana's web-service.yml to web.service.yml
    - Change type of prometheus-web to NodePort
    - Update service name and set port number within
      the URL
    - Make the pod query return only working pods on
      the spring-dashboard

--- k8s/setup.bat
+++ k8s/setup.bat
@@ -15,7 +15,7 @@ if errorlevel 1 exit /b %errorlevel%
 kubectl create configmap backend ^
 	--from-literal=conn-str=postgresql://postgres-0.db:5432/vpn
 kubectl create configmap frontend ^
-	--from-literal=BACKEND_BASE_URL=http://backend:8177/api
+	--from-literal=BACKEND_BASE_URL=http://spring:8177/api
 
 for %%s in (postgres spring react prometheus grafana) do (
 	%SCRIPT_DIR%..\%%s\k8s\setup.bat
